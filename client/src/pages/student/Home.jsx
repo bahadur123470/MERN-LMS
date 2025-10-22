12a654030aa392d468845655e2d242958f96cc8c
Bahadur Ali <122412801+bahadur123470@users.noreply.github.com>
2025-10-22 21:14:03 +0500

    feat: Companies component configured to Home page

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Hero from '../../components/students/Hero'
+import Hero from '../../components/students/Hero.jsx'
+import Companies from '../../components/students/Companies.jsx'
 
 const Home = () => {
     return (
         <div className='flex flex-col items-center space-y-7'>
             <Hero/>
+            <Companies/>
         </div>
     )
 }
